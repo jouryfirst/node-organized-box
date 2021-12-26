b67ad86c883cb1e65e1a82951cebe9a2f83e0558
jouryzhang <jouryzhang@gmail.com>
2021-12-26 22:05:56 +0800

    [MOD] 增加依赖sequelize、jest

---
+++ src/db/seq.js
@@ -0,0 +1,20 @@
+/**
+ * @description sequelize实例
+ * @author joury
+ */
+
+const Sequelize = require('sequelize')
+
+const conf = {
+  host: 'localhost',
+  port: 3306,
+  dialect: 'mysql'
+}
+
+const seq = new Sequelize('kod2_organized_box', 'root', 'wwwnode1013+', conf)
+
+seq.authenticate().then(() => {
+  console.log('ok')
+}).catch((e) => {
+  console.log('err', e)
+})
